7d4c5a65c421942a1b52e6ada060c499a6d47a5d
Alexander Guerrero <alex.guelu@gmail.com>
2020-10-05 23:20:50 -0500

    043 Uso del método save de Hibernate

--- src/com/alex/dao/AdminDaoImpl.java
+++ src/com/alex/dao/AdminDaoImpl.java
@@ -2,14 +2,28 @@ package com.alex.dao;
 
 import java.util.List;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
 import com.alex.pojo.Admin;
 
+@Transactional
+@Repository
 public class AdminDaoImpl implements AdminDao {
+	
+	@Autowired
+	private SessionFactory sessionFactory;
+	
+	public Session getSession() {
+		return sessionFactory.getCurrentSession();
+	}
 
 	@Override
 	public void save(Admin admin) {
-		// TODO Auto-generated method stub
-		
+		getSession().save(admin);
 	}
 
 	@Override
